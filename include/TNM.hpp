990fdebd523a67df75d898224776b942f84d23db
Lucas Guillerot lguiller <lguiller@student.42.fr>
2022-01-20 16:46:57 +0100

    creation de la fonction print_surface, reduction au minimum de l'appel a print_surface et creation du dossier output si il n'existe pas

--- include/TNM.hpp
+++ include/TNM.hpp
@@ -6,15 +6,17 @@
 # include <SDL2/SDL_image.h>
 # include <iostream>
 # include <ctime>
+# include <filesystem>
 
-# define MUSIC_PATH "./sounds/clearly.ogg"
-# define WINDOW_W 800
-# define WINDOW_H 600
-# define RECT_SIZE 20
-# define TIME_BEFORE_CHANGE 5
-# define RMASK 0xff000000
-# define GMASK 0xff0000
-# define BMASK 0xff00
-# define AMASK 0xff
+# define MUSIC_PATH			"./sounds/clearly.ogg"
+# define RECT_SIZE			20
+# define TIME_BEFORE_CHANGE	5
+# define RMASK				0xff000000
+# define GMASK				0xff0000
+# define BMASK				0xff00
+# define AMASK				0xff
+# define BLACK				0xff
+# define RED				0xff0000ff
+# define WHITE				0xffffffff
 
 #endif
--- src/TNM.cpp
+++ src/TNM.cpp
@@ -6,7 +6,7 @@
 /*   By: lynux <marvin@42.fr>                       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/01/20 13:32:07 by lynux             #+#    #+#             */
-/*   Updated: 2022/01/20 15:40:26 by lynux            ###   ########.fr       */
+/*   Updated: 2022/01/20 16:44:49 by lynux            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,12 +17,12 @@ void	SDL_ExitWithError(const char *message, const char *error) {
 	exit(1);
 }
 
-void	init(SDL_Window **window, SDL_Renderer **renderer, SDL_Surface **background, SDL_Surface **square, Mix_Music **sound) {
-	if ((*window = SDL_CreateWindow("Test de la négligence sensorielle", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, WINDOW_W, WINDOW_H, SDL_WINDOW_RESIZABLE)) == NULL)
+void	init(SDL_DisplayMode screen, SDL_Window **window, SDL_Renderer **renderer, SDL_Surface **background, SDL_Surface **square, Mix_Music **sound) {
+	if ((*window = SDL_CreateWindow("Test de la négligence sensorielle", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, screen.w, screen.h, SDL_WINDOW_FULLSCREEN)) == NULL)
 		SDL_ExitWithError("Error Create window", SDL_GetError());
 	if ((*renderer = SDL_CreateRenderer(*window, -1, 0)) == NULL)
 		SDL_ExitWithError("Error Create renderer", SDL_GetError());
-	if ((*background = SDL_CreateRGBSurface(0, WINDOW_W, WINDOW_H, 32, RMASK, GMASK, BMASK, AMASK)) == NULL)
+	if ((*background = SDL_CreateRGBSurface(0, screen.w, screen.h, 32, RMASK, GMASK, BMASK, AMASK)) == NULL)
 		SDL_ExitWithError("Error Create background", SDL_GetError());
 	if ((*square = SDL_CreateRGBSurface(0, RECT_SIZE, RECT_SIZE, 32, RMASK, GMASK, BMASK, AMASK)) == NULL)
 		SDL_ExitWithError("Error Create square", SDL_GetError());
@@ -30,18 +30,22 @@ void	init(SDL_Window **window, SDL_Renderer **renderer, SDL_Surface **background
 		SDL_ExitWithError("Error Load music", Mix_GetError());
 }
 
-void	draw_line(SDL_Renderer **renderer, SDL_Surface **background, SDL_MouseMotionEvent motion) {
+void	print_surface(SDL_Renderer **renderer, SDL_Surface *surface, SDL_Rect *dest) {
 	SDL_Texture	*texture = NULL;
-	SDL_Rect	rect = {motion.x, motion.y, 1, 1};
 
-	SDL_FillRect(*background, &rect, 0xffffffff);
-	texture = SDL_CreateTextureFromSurface(*renderer, *background);
-	SDL_RenderCopy(*renderer, texture, NULL, NULL);
+	texture = SDL_CreateTextureFromSurface(*renderer, surface);
+	SDL_RenderCopy(*renderer, texture, NULL, dest);
 	SDL_DestroyTexture(texture);
 }
 
-void	change_target_pos(SDL_Rect *target, std::time_t *timestamp, Mix_Music *sound) {
-	*target = {rand() % (WINDOW_W - RECT_SIZE), rand() % (WINDOW_H - RECT_SIZE), RECT_SIZE, RECT_SIZE};
+void	draw_line(SDL_Renderer **renderer, SDL_Surface **background, SDL_MouseMotionEvent motion) {
+	SDL_Rect	rect = {motion.x, motion.y, 1, 1};
+
+	SDL_FillRect(*background, &rect, WHITE);
+}
+
+void	change_target_pos(SDL_DisplayMode screen, SDL_Rect *target, std::time_t *timestamp, Mix_Music *sound) {
+	*target = {rand() % (screen.w - RECT_SIZE), rand() % (screen.h - RECT_SIZE), RECT_SIZE, RECT_SIZE};
 	*timestamp = time(nullptr);
 	Mix_PlayMusic(sound, 1);
 }
@@ -51,12 +55,11 @@ int		main(int ac, char **av) {
 	SDL_Renderer	*renderer	= NULL;
 	SDL_Surface		*background	= NULL;
 	SDL_Surface		*square		= NULL;
-	SDL_Texture		*texture	= NULL;
+	SDL_DisplayMode	screen;
 	SDL_Event		event;
 	SDL_Rect		target;
 	Mix_Music		*sound;
 	std::time_t		timestamp = time(nullptr);
-//	SDL_DisplayMode	DM;
 	bool			stop;
 
 	srand(time(nullptr));
@@ -64,11 +67,10 @@ int		main(int ac, char **av) {
 		SDL_ExitWithError("Error Initialisation SDL", SDL_GetError());
 	if (Mix_OpenAudio(44100, MIX_DEFAULT_FORMAT, 2, 1024) != 0)
 		SDL_ExitWithError("Error Open audio", Mix_GetError());
-	init(&window, &renderer, &background, &square, &sound);
-	SDL_FillRect(background, NULL, 0xff);
-	texture = SDL_CreateTextureFromSurface(renderer, background);
-	SDL_RenderCopy(renderer, texture, NULL, NULL);
-	change_target_pos(&target, &timestamp, sound);
+	SDL_GetCurrentDisplayMode(0, &screen);
+	init(screen, &window, &renderer, &background, &square, &sound);
+	SDL_FillRect(background, NULL, BLACK);
+	change_target_pos(screen, &target, &timestamp, sound);
 	while (!stop) {
 		while (SDL_PollEvent(&event)) {
 			if (event.type == SDL_QUIT || (event.type == SDL_KEYDOWN && event.key.keysym.sym == SDLK_ESCAPE))
@@ -77,22 +79,19 @@ int		main(int ac, char **av) {
 				draw_line(&renderer, &background, event.motion);
 				if (event.motion.x >= target.x && event.motion.y >= target.y &&
 						event.motion.x <= target.x + target.w && event.motion.y <= target.y + target.h)
-					change_target_pos(&target, &timestamp, sound);
+					change_target_pos(screen, &target, &timestamp, sound);
 			}
 			if (event.type == SDL_KEYDOWN && event.key.keysym.sym == SDLK_SPACE) {
+				std::filesystem::create_directory("./output");
 				IMG_SavePNG(background, "./output/out.png");
 				stop = true;
 			}
 		}
-		if (time(nullptr) - timestamp >= 5)
-			change_target_pos(&target, &timestamp, sound);
-		texture = SDL_CreateTextureFromSurface(renderer, background);
-		SDL_RenderCopy(renderer, texture, NULL, NULL);
-		SDL_DestroyTexture(texture);
-		SDL_FillRect(square, NULL, 0xff0000ff);
-		texture = SDL_CreateTextureFromSurface(renderer, square);
-		SDL_RenderCopy(renderer, texture, NULL, &target);
-		SDL_DestroyTexture(texture);
+		if (time(nullptr) - timestamp >= TIME_BEFORE_CHANGE)
+			change_target_pos(screen, &target, &timestamp, sound);
+		print_surface(&renderer, background, NULL);
+		SDL_FillRect(square, NULL, RED);
+		print_surface(&renderer, square, &target);
 		SDL_RenderPresent(renderer);
 	}
 	SDL_DestroyWindow(window);
